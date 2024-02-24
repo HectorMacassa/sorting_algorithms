d6bf1fd2e88695f4edf8112efb5fa09cdea3cb1d
Hector Macassa <cbmacassa@gmail.com>
2024-02-24 13:23:05 -0800

    Shell sort - Knuth Sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,7 +12,7 @@ void shell_sort(int *array, size_t size)
 	while (interval < size / 3)
 		interval = interval * 3 + 1;
 
-	while (interval > 0)
+	for (; interval > 0; interval /= 3)
 	{
 		for (i = interval; i < size; i++)
 		{
@@ -26,7 +26,6 @@ void shell_sort(int *array, size_t size)
 			}
 			array[j] = temp;
 		}
-		interval = (interval - 1) / 3;
+		print_array(array, size);
 	}
-	print_array(array, size);
 }
