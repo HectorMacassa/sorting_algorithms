30d2f75a142119201ea8f3bf75ad065d923d0115
Hector Macassa <cbmacassa@gmail.com>
2024-02-24 13:08:06 -0800

    Shell sort - Knuth Sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * shell_sort - Sorts an array of integers in ascending order using Shell Sort
+ * @array: the array of integers to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t interval = 1, i, j;
+	int temp;
+
+	while (interval < size / 3)
+		interval = interval * 3 + 1;
+
+	while (interval > 0)
+	{
+		for (i = interval; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= interval && array[j - interval] > temp)
+			{
+				array[j] = array[j - interval];
+				j -= interval;
+			}
+			array[j] = temp;
+		}
+		interval = (interval - 1) / 3;
+	}
+	print_array(array, size);
+}
